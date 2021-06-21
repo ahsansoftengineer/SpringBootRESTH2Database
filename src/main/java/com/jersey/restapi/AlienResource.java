9f40f5bdd49aae014b3a0d74f96badadbae14791
Muhammad Ahsan <mahsansoftengineer@indusvalley.io>
2021-06-21 19:32:43 +0500

    Removed unnessary Imports

--- src/main/java/com/jersey/restapi/AlienResource.java
+++ src/main/java/com/jersey/restapi/AlienResource.java
@@ -1,19 +1,20 @@
 package com.jersey.restapi;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+// Commented due to causing error several time
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
 
-@Path("aliens")
+//@Path("aliens")
 public class AlienResource {
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public Alien getAlien(){
-        System.out.println("Get Alien Called");
-        Alien a1 = new Alien();
-        a1.setName("John");
-        a1.setPoints(78);
-        return a1;
-    }
+//    @GET
+//    @Produces(MediaType.APPLICATION_XML)
+//    public Alien getAlien(){
+//        System.out.println("Get Alien Called");
+//        Alien a1 = new Alien();
+//        a1.setName("John");
+//        a1.setPoints(78);
+//        return a1;
+//    }
 }
